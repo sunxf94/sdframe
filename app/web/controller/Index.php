7eab57890ef1a2ed22feb1a1c14203f139606245
sunxuefeng <snowin1994@foxmail.com>
2020-03-24 19:03:22 +0800

    增加时间、昵称显示

--- app/web/controller/Index.php
+++ app/web/controller/Index.php
@@ -3,11 +3,18 @@
 namespace app\web\controller;
 
 use services\TalkService;
+use services\UserService;
 
 class Index extends Base {
 
     public function index() {
         $talkList = TalkService::getList();
+        foreach ($talkList as &$talk) {
+            $fromUser = UserService::getUserByID($talk['from_id']);
+            $talk['from_name'] = $fromUser['nickname'];
+            $toUser = UserService::getUserByID($talk['to_id']);
+            $talk['to_name'] = $toUser['nickname'];
+        }
 
         SDF()->assign('list', $talkList);
         $this->setTemplate("index/index");
---
+++ services/UserService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace services;
+
+class UserService {
+    public static function getUserByID($id) {
+        if (!$id) {
+            throw new \Exception('invalid id');
+        }
+
+        $where = ['status' => 1, 'id' => $id];
+        $user = SDF()->db->get('users', '*', $where);
+        if (!$user) {
+            throw new \Exception('invalid user');
+        }
+
+        return $user;
+    }
+}
